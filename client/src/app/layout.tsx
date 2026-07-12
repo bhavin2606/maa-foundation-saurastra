3766c671e2ee5812f47df9a472b7fa0666ccca95
bhavin padiya <bhavin.padiya@merai.co>
2026-07-12 14:22:41 +0530

    feat: integrate FloatingWhatsApp component into the global layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./Providers";
+import FloatingWhatsApp from "@/components/common/FloatingWhatsApp";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
     >
       <body className="flex min-h-screen flex-col">
         <Providers>{children}</Providers>
+        <FloatingWhatsApp />
         <script src="https://checkout.razorpay.com/v1/checkout.js" async></script>
       </body>
     </html>
